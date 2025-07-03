19dd2386e6edd7782838eea2376b00ed3ef49bb4
Ragu <ragu.n@constient.com>
2025-07-03 15:34:24 +0530

    Refactor: config changes

--- app/utils/config.js
+++ app/utils/config.js
@@ -1,6 +1,6 @@
 const config = {
-    BASE_URL: process.env.REACT_APP_BASE_URL || "https://bce3-2406-7400-c8-b3a8-7425-f1b8-e9ad-d1a1.ngrok-free.app",
-    IFRAME_URL: process.env.APP_IFRAME_URL || "http://localhost:8077/shopify/"
+    BASE_URL: process.env.REACT_APP_BASE_URL || "https://cli.sup-uni.com",
+    IFRAME_URL: process.env.APP_IFRAME_URL || "https://app.sup-uni.com/shopify/"
 }
 console.log("Loaded BASE_URL:", config.BASE_URL);
 export default  config;
--- extensions/config.js
+++ extensions/config.js
@@ -1,4 +1,4 @@
 export default {
     AppName: "SU Sales New",
-    baseURL: "https://db76-2406-7400-c8-b3a8-7425-f1b8-e9ad-d1a1.ngrok-free.app"
+    baseURL: "https://api.sup-uni.com"
 };
