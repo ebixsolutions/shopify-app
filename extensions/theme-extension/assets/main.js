41a473f54cc89415d93ef81513bdf59c062199d1
Manoj Prabhu <manojprabu@constient.com>
2026-04-22 17:24:06 +0530

    feat: referral undefined issue fixes

--- extensions/theme-extension/assets/main.js
+++ extensions/theme-extension/assets/main.js
@@ -344,6 +344,7 @@ window.onload = () => {
                     if (res.data) {
 
                         const data = res.data;
+                        const shopUrl = window.location.origin;
                         if (data.membership) {
                             $("#ebix-tier").text(data.membership.name || "-");
                         }
@@ -372,7 +373,7 @@ window.onload = () => {
 
                         $("#ebix-pro-ref-code").text(data.invite_code || "");
 
-                        $("#refer-friend #facebook").attr("href", encodeURI(`https://www.facebook.com/sharer/sharer.php?u=${window.shopUrl}&t=Register and use this code: ${data.invite_code}`));
+                        $("#refer-friend #facebook").attr("href", encodeURI(`https://www.facebook.com/sharer/sharer.php?u=${shopUrl}&t=Register and use this code: ${data.invite_code}`));
     
 
                         $("#refer-friend #email").click((e) => {
@@ -427,7 +428,7 @@ window.onload = () => {
                             ebix_generate_table(referral_rule, "ebix-ref-reward", ebix_Promotion.CustomerId && !referral_illigible);
                             const awardText = referral_rule[0]?.award || "";
                             $("#refer-friend #whatsapp").attr("href", encodeURI(
-                                `https://api.whatsapp.com/send?text=${window.shopUrl} \nSignup with code: ${data.invite_code} \nto ${awardText}`
+                                `https://api.whatsapp.com/send?text=${shopUrl} \nSignup with code: ${data.invite_code} \nto ${awardText}`
                             ));
                         }
                         if (referral_rule.length > 0 || referral_history.length > 0 || data.reward != null) {
