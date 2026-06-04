9e1e9c920a6186b0aec1baad9993d54e6cd43123
Manoj Prabhu <manojprabu@constient.com>
2026-06-04 11:08:09 +0530

    feat: shopify server config and extensions add

--- app/utils/config.js
+++ app/utils/config.js
@@ -1,6 +1,6 @@
 const config = {
-    BASE_URL: process.env.REACT_APP_BASE_URL || "https://cli.sup-uni.com",
-    IFRAME_URL: process.env.APP_IFRAME_URL || "https://app.sup-uni.com/shopify/"
+    BASE_URL: process.env.REACT_APP_BASE_URL || "https://penna-cement.constient.com",
+    IFRAME_URL: process.env.APP_IFRAME_URL || "https://b2c1.constient.com/shopify/"
 }
 console.log("Loaded BASE_URL:", config.BASE_URL);
 export default  config;
--- extensions/config.js
+++ extensions/config.js
@@ -1,4 +1,4 @@
 export default {
-    AppName: "SU Sales New",
-    baseURL: "https://api.sup-uni.com"
+    AppName: "SUSales_local",
+    baseURL: "https://b2c.constient.com"
 };
